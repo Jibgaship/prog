ebd6f327cf43af23f0bc8f3f7684ea6f2a5dd4ee
Jibgaship <85870364+Jibgaship@users.noreply.github.com>
2022-08-11 15:08:56 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int T, num1, num2;
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++)
+	{
+		scanf("%d %d", &num1, &num2);
+		printf("Case #%d: %d + %d = %d\n", i + 1, num1, num2, num1 + num2);
+	}
+	return 0;
+}
