6704afe48cdaa672e77e015a28375f425ebf5686
Jibgaship <85870364+Jibgaship@users.noreply.github.com>
2023-05-06 00:27:44 +0900

    [unrated] Title: 정수 부분, Time: 0.01 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(double flo) {
+    int answer = 0;
+    answer = flo;
+    return answer;
+}
+
+int main() {
+    double flo;
+    scanf("%lf", &flo);
+    printf("%d", solution(flo));
+    
+    return 0;
+}
